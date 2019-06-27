a8c63279db2ee2cb21652a4feabaeed3220327aa
Alejandro <alejandro.lvd@gmail.com>
2019-06-27 19:00:30 -0400

    refactor de nuevo

--- Proyeccion_Esfera.java
+++ Proyeccion_Esfera.java
@@ -11,7 +11,6 @@ import ij.measure.Calibration;
 import ij.measure.ResultsTable;
 
 import java.util.Vector;
-
 import java.awt.*;
 
 public class Proyeccion_Esfera implements ExtendedPlugInFilter, DialogListener {
@@ -21,11 +20,8 @@ public class Proyeccion_Esfera implements ExtendedPlugInFilter, DialogListener {
           DOES_ALL |              //this plugin processes 8-bit, 16-bit, 32-bit gray & 24-bit/pxl RGB
           KEEP_PREVIEW;           //When using preview, the preview image can be kept as a result
 
-  private double percentage;      //how much of the other slice is shown
-  private ImageProcessor otherSlice;  //Image data of the other slice
   protected ImageStack stack;
-  static double factor;
-  static double pelar;
+  private double pelar;
   private boolean preview = false;
   private boolean kkk = false;
   private ImagePlus imp;
@@ -37,9 +33,7 @@ public class Proyeccion_Esfera implements ExtendedPlugInFilter, DialogListener {
   private double zpre;
   private double rpre;
 
-  private int width;
-  private int height;
-  private int zs;
+  private int width, height, zs, tiempos;
 
   private double factorx;
   private double factory;
@@ -58,131 +52,77 @@ public class Proyeccion_Esfera implements ExtendedPlugInFilter, DialogListener {
    *            See PlugInFilter.java and ExtendedPlugInFilter in the ImageJ
    *            sources for details.
    */
-  public int setup (String arg, ImagePlus imp) {
+  public int setup(String arg, ImagePlus imp) {
       return FLAGS;
   }
 
-    /** Ask the user for the parameters. This method of an ExtendedPlugInFilter
-     *  is called by ImageJ after setup.
-     * @param imp       The ImagePlus containing the image (or stack) to process.
-     * @param command   The ImageJ command (as it appears the menu) that has invoked this filter
-     * @param pfr       A reference to the PlugInFilterRunner, needed for preview
-     * @return          Flags, i.e. a code describing supported formats etc.
-     */
-  public int showDialog (ImagePlus imp, String command, PlugInFilterRunner pfr) {
-        
-    System.out.println("FLAG para ShowDialog ");
+  /** Ask the user for the parameters. This method of an ExtendedPlugInFilter
+   *  is called by ImageJ after setup.
+   * @param imp       The ImagePlus containing the image (or stack) to process.
+   * @param command   The ImageJ command (as it appears the menu) that has invoked this filter
+   * @param pfr       A reference to the PlugInFilterRunner, needed for preview
+   * @return          Flags, i.e. a code describing supported formats etc.
+   */
+  public int showDialog(ImagePlus imp, String command, PlugInFilterRunner pfr) {
+    System.out.println("FLAG para ShowDialog");
+    
+    Calibration cal = imp.getCalibration(); 
+    factorx = cal.pixelWidth; //x contains the pixel width in units
+    factory = cal.pixelHeight; //y contains the pixel height in units
+    factorz = cal.pixelDepth; //z contains the pixel (voxel) depth in units
+
     preview = true;
-    imp = WindowManager.getCurrentImage(); 
-    ImagePlus img2 = imp;
-      
-    stack = img2.getStack();    
+    
+    stack = imp.getStack();    
     direc = imp.getOriginalFileInfo().directory;
     imagedir = imp.getOriginalFileInfo().fileName;
     
-    System.out.println("Directorio de la imagen"+ direc);
-    System.out.println("Directorio de la imagen"+ imagedir);
-    
-    int tiempos = img2.getNFrames(); //tiempos a procesar
-    
-    zs = stack.getSize()/tiempos; // numero de planos z
+    tiempos = imp.getNFrames(); //tiempos a procesar
+    zs = stack.getSize() / tiempos; // numero de planos z
     width = stack.getWidth(); //ancho de imagen
     height = stack.getHeight(); //alto de imagen
     
-    //  byte[] pixels;  //arreglo para guardar slices del stack
-    byte[] pix_est; //para estimar el centro y radio de la esfera
-    
-    int x, offset, offsetaux, pos;  //para buscar coordenadas en el stack 
-    double distancia;
-    
     //stack auxiliar para guardar stack proyectado
-    ImageStack stack_proyeccion = new ImageStack(width, height);    
-    
-    //IJ.showMessage("Numero de tiempos:  " + tiempos + " \nNumero de planos:  " + zs);
-    
-    //centro y radio de la esfera
-    double[] x0 = new double[tiempos];
-    double[] y0 = new double[tiempos];
-    double[] z0 = new double[tiempos];
-    double[] r = new double[tiempos];
-    
-    //conversion de voxeles a micrones
-    Calibration cal = imp.getCalibration(); 
-        double factorx = cal.pixelWidth; //x contains the pixel width in units 
-        double factory = cal.pixelHeight; //y contains the pixel height in units 
-        double factorz = cal.pixelDepth; //z contains the pixel (voxel) depth in units 
-
-    img2.getProcessor().setAutoThreshold(AutoThresholder.Method.Otsu, true);
-    
-    double threshold = img2.getProcessor().getMinThreshold();
-    
-    double factor_otsu = Math.pow(2, (int) threshold/2)/((int) threshold/2 - 1);
+    imp.getProcessor().setAutoThreshold(AutoThresholder.Method.Otsu, true);
+    double threshold = imp.getProcessor().getMinThreshold();
+    int factor_otsu = (int)Math.pow(2, (int)threshold / 2) / ((int)threshold / 2 - 1);
     
     //dimension de arreglos para guardar datos
-    int dimension = (int) width*height*zs/15;
+    int dimension = width * height * zs / 15;
     
     //arreglos para guardar datos para estimacion de la esfera
     double[] mx = new double[dimension];
     double[] my = new double[dimension];
     double[] mz = new double[dimension];
     
-    double[] datos;
-    int cnt = 0;
-
-    double rmax = 0;
-    double x_z0, y_z0, x_zs, y_zs, r_z0, r_zs;
-
     //circulo para z=0
-    pix_est = (byte[]) stack.getPixels(1);
-    for (int i=0; i < height; i++) {
-      offsetaux = i*width;
-      for (int j=0; j < width; j++) {
-        pos = offsetaux + j;
-        if ((pix_est[pos]) > (int) factor_otsu || pix_est[pos] < 0) {
-          mx[cnt] = j;
-          my[cnt] = i;
-          cnt += 1;
-        }
-      }
-    }
-      
-    datos = circle_estimation(mx,my,cnt, imp);
-      
+    int cnt = pixelesSobreUmbral(factor_otsu, 1, mx, my, mz);
+    
+    double[] datos = circle_estimation(mx, my, cnt, imp);
     x_z0_pre = datos[0];
     y_z0_pre = datos[1];
     r_z0_pre = datos[2];
-    cnt = 0;
+
     //circulo para z=zs
-    pix_est = (byte[]) stack.getPixels(zs);
-    for (int i=0; i < height; i++) {
-      offsetaux = i*width;
-      for (int j=0; j < width; j++) {
-        pos = offsetaux + j;
-        if ((pix_est[pos]) > (int) factor_otsu || pix_est[pos] < 0) {
-          mx[cnt] = j;
-          my[cnt] = i;
-          cnt += 1;
-        }
-      }
-    }
+    cnt = pixelesSobreUmbral(factor_otsu, zs, mx, my, mz);
     
-    datos = circle_estimation(mx,my,cnt, imp);
+    datos = circle_estimation(mx, my, cnt, imp);
     x_zs_pre = datos[0];
     y_zs_pre = datos[1];
     r_zs_pre = datos[2];
     cnt = 0;
     
     // estimar esfera
-    for (int h = 1; h <= zs; h++) {
-      pix_est = (byte[]) stack.getPixels(h + (1 - 1) * zs);
+    for (int z = 1; z <= zs; z++) {
+      byte[] pix_est = (byte[]) stack.getPixels(z);
       for (int i = 0; i < height; i++) {
-        offsetaux = i * width;
+        int offsetaux = i * width;
         for (int j = 0; j < width; j++) {
-          pos = offsetaux + j;
-          if ((pix_est[pos]) > (int) factor_otsu || pix_est[pos] < 0) {
+          int pos = offsetaux + j;
+          if ((pix_est[pos]) > factor_otsu || pix_est[pos] < 0) {
             mx[cnt] = j;
             my[cnt] = i;
-            mz[cnt] = h - 1;
+            mz[cnt] = z - 1;
             cnt += 1;
           }
         }
@@ -190,11 +130,10 @@ public class Proyeccion_Esfera implements ExtendedPlugInFilter, DialogListener {
     }
     
     datos = sphere_estimation(mx, my, mz, cnt, imp);
-    
     xpre = datos[0];
     ypre = datos[1];
     zpre = datos[2];
-    rpre  = datos[3];
+    rpre = datos[3];
 
     GenericDialog gd = new GenericDialog("Factor de corte");
     gd.addSlider("Ingrese un numero entre 0 y 1", 0.0, 1.0, 0.94);
@@ -211,6 +150,24 @@ public class Proyeccion_Esfera implements ExtendedPlugInFilter, DialogListener {
     return FLAGS;
   }
 
+  private int pixelesSobreUmbral(int umbral, int z, double[] cx, double[] cy, double[] cz) {
+    byte[] pix_est = (byte[]) stack.getPixels(z);
+    int cnt = 0;
+    for (int i = 0; i < height; i++) {
+      int offsetaux = i * width;
+      for (int j = 0; j < width; j++) {
+        int pos = offsetaux + j;
+        if (pix_est[pos] > umbral || pix_est[pos] < 0) {
+          cx[cnt] = j;
+          cy[cnt] = i;
+          cz[cnt] = z - 1;
+          cnt += 1;
+        }
+      }
+    }
+    return cnt;
+  }
+
   /** Listener to modifications of the input fields of the dialog.
    *  Here the parameters should be read from the input dialog.
    *  @param gd The GenericDialog that the input belongs to
@@ -219,24 +176,21 @@ public class Proyeccion_Esfera implements ExtendedPlugInFilter, DialogListener {
    */
   public boolean dialogItemChanged (GenericDialog gd, AWTEvent e) {
     System.out.println("FLAG para DialogItemChanged ");
-    
-    pelar = (double) gd.getNextNumber(); 
-      //System.out.println("El valor de pelar es " + pelar);
-    
-    return !gd.invalidNumber() && pelar >=0  &&  pelar <= 1;
+    pelar = (double) gd.getNextNumber();     
+    return !gd.invalidNumber() && pelar >= 0 && pelar <= 1;
   }
 
   /**
    * This method is called by ImageJ for processing
    * @param ip The image that should be processed
    */
-  public void run (ImageProcessor ip) {
+  public void run(ImageProcessor ip) {
     System.out.println("FLAG para RUN ");
     
     if (preview) {
-      System.out.println("Se calculara con este valor de pelado"+ pelar);  
-        
-      int  offset; // para buscar coordenadas en el stack
+      System.out.println("Se calculara con este valor de pelado" + pelar);  
+      
+      int offset; // para buscar coordenadas en el stack
       double distancia = 0;
       //ImageStack stack_proyeccion = new ImageStack(width, height);
       ImagePlus imageprev = IJ.openVirtual(direc + imagedir);
@@ -253,7 +207,7 @@ public class Proyeccion_Esfera implements ExtendedPlugInFilter, DialogListener {
       System.out.println("Paso 2");
       ColorProcessor ipc;
 
-      for (int h=1; h <= zs ; h++) {
+      for (int h = 1; h <= zs; h++) {
         ImageProcessor histIp = new ByteProcessor(width, height);
         for (int i = 0; i < height ; i++) {
           for (int j = 0; j < width; j++) {
@@ -321,10 +275,8 @@ public class Proyeccion_Esfera implements ExtendedPlugInFilter, DialogListener {
       //duplicar para no modificar stack original
       ImagePlus img2 = new Duplicator().run(imp);
       
-      stack = img2.getStack();
-      
-      int tiempos = img2.getNFrames(); //tiempos a procesar
-      
+      stack = img2.getStack();      
+      int tiempos = img2.getNFrames(); //tiempos a procesar      
       int zs = stack.getSize()/tiempos; // numero de planos z
       int width = stack.getWidth(); //ancho de imagen
       int height = stack.getHeight(); //alto de imagen
@@ -343,12 +295,6 @@ public class Proyeccion_Esfera implements ExtendedPlugInFilter, DialogListener {
       double[] y0 = new double[tiempos];
       double[] z0 = new double[tiempos];
       double[] r = new double[tiempos];
-      
-      //conversion de voxeles a micrones
-      Calibration cal = imp.getCalibration(); 
-      double factorx = cal.pixelWidth; //x contains the pixel width in units 
-      double factory = cal.pixelHeight; //y contains the pixel height in units 
-      double factorz = cal.pixelDepth; //z contains the pixel (voxel) depth in units
 
       img2.getProcessor().setAutoThreshold(AutoThresholder.Method.Otsu, true);
       
@@ -508,24 +454,17 @@ public class Proyeccion_Esfera implements ExtendedPlugInFilter, DialogListener {
     System.out.println("FLAG para setNpasses");
   }
   
-  public double dist(double[] xx, double[] yy, double width, double height, ImagePlus imp) {
-    Calibration cal = imp.getCalibration(); 
-    double factorx = cal.pixelWidth; //x contains the pixel width in units 
-    double factory = cal.pixelHeight; //y contains the pixel height in units 
-    double factorz = cal.pixelDepth; //z contains the pixel (voxel) depth in units 
-        
-    return (Math.sqrt(Math.pow(((xx[0]-yy[0])*factorx),2) + Math.pow(((xx[1]-yy[1])*factory),2)
-            + Math.pow(((xx[2]-yy[2])*factorz),2)));
+  private double realDist(double dx, double dy, double dz) {
+    return Math.sqrt(Math.pow(dx * factorx, 2) + Math.pow(dy * factory, 2) + Math.pow(dz * factorz, 2));
+  }
+  
+  private double realDist(double dx, double dy) {
+    return realDist(dx, dy, 0);
   }
   
   // El metodo asume que los puntos no son todos coplanares
   // y que estan aproximadamente en el borde
   public double[] sphere_estimation(double[] x, double[] y, double[] z, int contador, ImagePlus imp) {
-
-    Calibration cal = imp.getCalibration(); 
-    double factorx = cal.pixelWidth; //x contains the pixel width in units 
-    double factory = cal.pixelHeight; //y contains the pixel height in units 
-    double factorz = cal.pixelDepth; //z contains the pixel (voxel) depth in units 
       
     double Lba = 0, Lbb = 0, Lbc = 0, Lb = 0;
     double xb = 0, yb = 0, zb = 0;
@@ -548,7 +487,7 @@ public class Proyeccion_Esfera implements ExtendedPlugInFilter, DialogListener {
     
     for (int h = 1; h <= 300; h++) {
       for (int i = 0; i < contador; i++) {
-          Li[i] = Math.sqrt(Math.pow((x[i] - a) * factorx, 2) + Math.pow((y[i] - b) * factory, 2) + Math.pow((z[i] - c) * factorz, 2));
+          Li[i] = realDist(x[i] - a, y[i] - b, z[i] - c);
           Lba += (a - x[i]) / (Li[i] * contador);
           Lbb += (b - y[i]) / (Li[i] * contador);
           Lbc += (c - z[i]) / (Li[i] * contador);
@@ -564,7 +503,7 @@ public class Proyeccion_Esfera implements ExtendedPlugInFilter, DialogListener {
     }
 
     for (int i=0; i < contador; i++) {
-      Li[i] = Math.sqrt(Math.pow((x[i] - a) * factorx, 2) + Math.pow((y[i] - b) * factory, 2) + Math.pow((z[i] - c) * factorz, 2));
+      Li[i] = realDist(x[i] - a, y[i] - b, z[i] - c);
       r += Li[i] / contador;
     }
 
@@ -572,61 +511,45 @@ public class Proyeccion_Esfera implements ExtendedPlugInFilter, DialogListener {
     return q;
   }
   
-  public double[] circle_estimation(double[] x, double[] y, int contador, ImagePlus imp) {
+  public double[] circle_estimation(double[] x, double[] y, int pointCount, ImagePlus imp) {
     
-    Calibration cal = imp.getCalibration(); 
-    double factorx = cal.pixelWidth; //x contains the pixel width in units 
-    double factory = cal.pixelHeight; //y contains the pixel height in units 
-    double Lba = 0, Lbb = 0, Lb = 0;
-    double xb = 0, yb = 0;
-    double a,b;
-    double r = 0;
-
-    for (int j=0; j < contador; j++) {
-      xb += x[j];
-      yb += y[j];
+    double avgX = 0, avgY = 0;
+    for (int j = 0; j < pointCount; j++) {
+      avgX += x[j];
+      avgY += y[j];
     }
+    avgX /= pointCount;
+    avgY /= pointCount;
 
-    double[] Li = new double[contador];
-    
-    xb = xb/contador;
-    yb = yb/contador;
-    a = xb; b = yb;
-    
-    //IJ.showMessage("Numero de muestras tomadas:  "+ (int) contador);
-    
-    for (int h = 1; h <= 300; h++) {
-      for (int i=0; i < contador; i++) {
-          Li[i] = Math.sqrt(Math.pow((x[i] - a) * factorx, 2) + Math.pow((y[i] - b) * factory, 2));
-          Lba += (a - x[i]) / (Li[i] * contador);
-          Lbb += (b - y[i]) / (Li[i] * contador);
-          Lb += Li[i] / contador;
+    double[] Li = new double[pointCount];
+    double a = avgX;
+    double b = avgY;
+    final int iters = 300;
+    for (int iter = 1; iter <= iters; iter++) {
+      double Lba = 0, Lbb = 0, Lb = 0;
+      for (int i = 0; i < pointCount; i++) {
+        Li[i] = realDist(x[i] - a, y[i] - b);
+        Lba += (a - x[i]) / (Li[i] * pointCount);
+        Lbb += (b - y[i]) / (Li[i] * pointCount);
+        Lb += Li[i] / pointCount;
       }
-      a = xb + Lb * Lba;
-      b = yb + Lb * Lbb;
-      Lba = 0;
-      Lbb = 0;
-      Lb = 0;
+      a = avgX + Lb * Lba;
+      b = avgY + Lb * Lbb;
     }
 
-    for (int i=0; i < contador; i++) {
-      Li[i] = Math.sqrt(Math.pow((x[i] - a) * factorx, 2) + Math.pow((y[i] - b) * factory, 2));
-      r += Li[i] / contador;
+    double r = 0;
+    for (int i = 0; i < pointCount; i++) {
+      Li[i] = realDist(x[i] - a, y[i] - b);
+      r += Li[i] / pointCount;
     }
-
-    double[] q = {a,b,r};
-    return q;   
+    return new double[] {a, b, r};
   }
 
   public static void main(String[] args) {
-    // NO AGREGAR MAS LINEAS EN EL MAIN
     new ImageJ();
-    System.out.println("Working Directory = " + System.getProperty("user.dir"));
-    //ImagePlus image = IJ.openVirtual("Z:/Eclipse/Fijithelium/examples/Stack/video_animal1.tif");
-    ImagePlus image = IJ.openVirtual("C:/Users/Alejandro/Desktop/java/epithelium-projection/examples/ejemplo_esfera.tif");
-
+    ImagePlus image = IJ.openVirtual(args[0]);
     IJ.runPlugIn(image, "Proyeccion_Esfera", "parameter=value");
     // image.show();
-     WindowManager.addWindow(image.getWindow());
+    WindowManager.addWindow(image.getWindow());
   }
 }
--- README.md
+++ README.md
@@ -1,11 +1,12 @@
+La imagen y el classpath se configuran en .vscode/launch.json
+
 Para compilar:
 
-'''
+`
 javac -cp "C:\Fiji.app\jars\ij-1.52n.jar" Proyeccion_Esfera.java
-'''
+`
 
-Para ejecutar:
-
-'''
-java -cp "C:\Fiji.app\jars\ij-1.52n.jar;." Proyeccion_Esfera
-'''
+Lo que he hecho
+- Formatear el codigo
+- Eliminar variables de instancia no usadas
+-
