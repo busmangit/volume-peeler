c50e235f6e5b0876d8b853ee7972d0a7df05ccb9
Alejandro <alejandro.lvd@gmail.com>
2019-07-04 12:37:02 -0400

    major refactorino

--- Proyeccion_Esfera.java
+++ Proyeccion_Esfera.java
@@ -17,21 +17,18 @@ public class Proyeccion_Esfera implements ExtendedPlugInFilter, DialogListener {
           DOES_ALL |              //this plugin processes 8-bit, 16-bit, 32-bit gray & 24-bit/pxl RGB
           KEEP_PREVIEW;           //When using preview, the preview image can be kept as a result
 
-  private ImageStack source;
   private byte[] sourcePixels;
   private float[] distanceMap;
   private ImagePlus sourceImagePlus;
-  private double pelar;
-  private boolean preview = false;
+  private double proportion;
+  private boolean preview = true;
   private boolean okPressed = false;
-  private boolean previewWindowInitialized = false;
-
   private HashMap<Double, ColorProcessor> previewCache;
   
   private double x_z0_pre, y_z0_pre, x_zs_pre, y_zs_pre, r_z0_pre, r_zs_pre;
   private double xpre, ypre, zpre, rpre;
   private double factorx, factory, factorz;
-  private int width, height, zs, tiempos, threshold, pixelsPerSlice;
+  private int width, height, nSlices, nFrames, threshold, pixelsPerSlice, pixelsPerFrame;
   
   /**
    * This method is called by ImageJ for initialization.
@@ -48,54 +45,59 @@ public class Proyeccion_Esfera implements ExtendedPlugInFilter, DialogListener {
     this.factorx = cal.pixelWidth; //x contains the pixel width in units
     this.factory = cal.pixelHeight; //y contains the pixel height in units
     this.factorz = cal.pixelDepth; //z contains the pixel (voxel) depth in units
-    this.preview = true;
     this.sourceImagePlus = imp;
-    this.source = imp.getStack();
-    this.tiempos = imp.getNFrames(); //tiempos a procesar
-    this.width = source.getWidth(); //ancho de imagen
-    this.height = source.getHeight(); //alto de imagen
+    this.width = imp.getWidth(); //ancho de imagen
+    this.height = imp.getHeight(); //alto de imagen
+    this.nFrames = imp.getNFrames(); //nFrames a procesar
+    this.nSlices = imp.getNSlices() / this.nFrames; // numero de planos z
     this.pixelsPerSlice = this.width * this.height;
-    this.zs = source.getSize() / this.tiempos; // numero de planos z
+    this.pixelsPerFrame = this.pixelsPerSlice * this.nSlices;
     this.previewCache = new HashMap<Double, ColorProcessor>();
-
-    this.sourcePixels = new byte[this.pixelsPerSlice * this.zs * this.tiempos];
-    for (int z = 1; z <= zs; z++) {
-      this.sourceImagePlus.setSlice(z);
-      byte[] pixels = (byte[])this.sourceImagePlus.getProcessor().getPixelsCopy();
-      int zoffset = this.pixelsPerSlice * (z - 1);
-      for (int i = 0; i < height; i++) {
-        int yoffset = i * width;
-        for (int j = 0; j < width; j++) {
-          this.sourcePixels[zoffset + yoffset + j] = pixels[yoffset + j];
-        }
-      }
-    }
-
-    this.sourceImagePlus.setSlice(zs / 2);
-    this.sourceImagePlus.getProcessor().setAutoThreshold(AutoThresholder.Method.Otsu, true);
-    double otsuThreshold = this.sourceImagePlus.getProcessor().getMinThreshold();
-    this.threshold = (int)Math.pow(2, (int)otsuThreshold / 2) / ((int)otsuThreshold / 2 - 1);
-
+    initSourcePixels();
+    initThreshold();
     runInitialEstimations(imp);
     return FLAGS;
   }
 
+  private void initSourcePixels() {
+    this.sourcePixels = new byte[this.pixelsPerSlice * this.nSlices * this.nFrames];
+    for (int slice = 1; slice <= this.nSlices; slice++) {
+      this.sourceImagePlus.setSlice(slice);
+      byte[] pixels = (byte[])this.sourceImagePlus.getProcessor().getPixelsCopy();
+      int zoffset = this.pixelsPerSlice * (slice - 1);
+      for (int i = 0; i < this.height; i++) {
+        int yoffset = i * this.width;
+        for (int j = 0; j < this.width; j++) {
+          this.sourcePixels[zoffset + yoffset + j] = pixels[yoffset + j];
+        }
+      }
+    }
+  }
+
+  private void initThreshold() {
+    int thresholdSampleSlice = this.nSlices / 2;
+    this.sourceImagePlus.setSlice(thresholdSampleSlice);
+    this.sourceImagePlus.getProcessor().setAutoThreshold(AutoThresholder.Method.Otsu, true);
+    double otsuThreshold = this.sourceImagePlus.getProcessor().getMinThreshold();
+    this.threshold = (int)Math.pow(2, (int)otsuThreshold / 2) / ((int)otsuThreshold / 2 - 1);
+  }
+
   private void runInitialEstimations(ImagePlus imp) {
     // arreglos para guardar datos para estimacion de la esfera
-    int dimension = width * height * zs;
+    int dimension = this.pixelsPerFrame / 15;
     double[] mx = new double[dimension];
     double[] my = new double[dimension];
     double[] mz = new double[dimension];
     
     // circulo para z=0
-    int cnt = pixelsOverThreshold(1, 0, mx, my, mz);
+    int cnt = pixelsOverThreshold(1, 1, 0, mx, my, mz);
     double[] datos = circleEstimation(mx, my, cnt);
     x_z0_pre = datos[0];
     y_z0_pre = datos[1];
     r_z0_pre = datos[2];
 
     // circulo para z=zs
-    cnt = pixelsOverThreshold(zs, 0, mx, my, mz);
+    cnt = pixelsOverThreshold(1, this.nSlices, 0, mx, my, mz);
     datos = circleEstimation(mx, my, cnt);
     x_zs_pre = datos[0];
     y_zs_pre = datos[1];
@@ -103,8 +105,8 @@ public class Proyeccion_Esfera implements ExtendedPlugInFilter, DialogListener {
     
     // estimar esfera
     cnt = 0;
-    for (int z = 1; z <= zs; z++) {
-      cnt += pixelsOverThreshold(z, cnt, mx, my, mz);
+    for (int slice = 1; slice <= this.nSlices; slice++) {
+      cnt += pixelsOverThreshold(1, slice, cnt, mx, my, mz);
     }
     datos = sphereEstimation(mx, my, mz, cnt);
     xpre = datos[0];
@@ -112,13 +114,20 @@ public class Proyeccion_Esfera implements ExtendedPlugInFilter, DialogListener {
     zpre = datos[2];
     rpre = datos[3];
 
-    this.distanceMap = new float[this.sourcePixels.length];
-    for (int i = 0; i < this.sourcePixels.length; i++) {
+    this.distanceMap = getDistanceMap(1, xpre, ypre, zpre);
+  }
+
+  private float[] getDistanceMap(int frame, double x, double y, double z) {
+    float[] map = new float[this.pixelsPerFrame];
+    int from = this.pixelsPerFrame * (frame - 1);
+    int to = this.pixelsPerFrame * frame;
+    for (int i = from; i < to; i++) {
       int cx = i % this.width;
       int cy = (i / this.width) % this.height;
       int cz = i / this.pixelsPerSlice;
-      this.distanceMap[i] = (float)realDist(cx - xpre, cy - ypre, cz - zpre);
+      map[i] = (float)realDist(cx - x, cy - y, cz - z);
     }
+    return map;
   }
 
   /** Ask the user for the parameters. This method of an ExtendedPlugInFilter
@@ -130,8 +139,8 @@ public class Proyeccion_Esfera implements ExtendedPlugInFilter, DialogListener {
    */
   public int showDialog(ImagePlus imp, String command, PlugInFilterRunner pfr) {
     System.out.println("FLAG para ShowDialog");
-    GenericDialog gd = new GenericDialog("Parámetros");
-    gd.addSlider("Factor de corte", 0.0, 1.0, 0.94);
+    GenericDialog gd = new GenericDialog("Sphere projection");
+    gd.addSlider("Radius proportion", 0.0, 1.0, 0.94);
     gd.addPreviewCheckbox(pfr);
     gd.addDialogListener(this);
     gd.showDialog();
@@ -143,16 +152,17 @@ public class Proyeccion_Esfera implements ExtendedPlugInFilter, DialogListener {
     return FLAGS;
   }
   
-  private int pixelsOverThreshold(int slice, int offset, double[] x, double[] y, double[] z) {
+  private int pixelsOverThreshold(int frame, int slice, int prevCnt, double[] x, double[] y, double[] z) {
+    int frameOffset = (frame - 1) * this.pixelsPerFrame;
     int cnt = 0;
     for (int i = 0; i < height; i++) {
-      int offsetaux = i * width;
+      int offsetaux = i * width + frameOffset;
       for (int j = 0; j < width; j++) {
         int pos = offsetaux + j;
         if (this.sourcePixels[(slice - 1) * this.pixelsPerSlice + pos] > threshold || this.sourcePixels[(slice - 1) * this.pixelsPerSlice + pos] < 0) {
-          x[offset + cnt] = j;
-          y[offset + cnt] = i;
-          z[offset + cnt] = slice - 1;
+          x[prevCnt + cnt] = j;
+          y[prevCnt + cnt] = i;
+          z[prevCnt + cnt] = slice - 1;
           cnt++;
         }
       }
@@ -168,8 +178,8 @@ public class Proyeccion_Esfera implements ExtendedPlugInFilter, DialogListener {
    */
   public boolean dialogItemChanged(GenericDialog gd, AWTEvent e) {
     System.out.println("FLAG para DialogItemChanged ");
-    pelar = (double) gd.getNextNumber();     
-    return !gd.invalidNumber() && pelar >= 0 && pelar <= 1;
+    this.proportion = (double) gd.getNextNumber();     
+    return !gd.invalidNumber() && this.proportion >= 0 && this.proportion <= 1;
   }
 
   /**
@@ -180,134 +190,88 @@ public class Proyeccion_Esfera implements ExtendedPlugInFilter, DialogListener {
     System.out.println("FLAG para RUN ");
     
     if (preview) {
-
-      if (this.previewCache.containsKey(new Double(pelar))) {
-        IJ.getImage().setImage(new ImagePlus("Preview", this.previewCache.get(new Double(pelar))));
-        return;
-      }
-
-      ImageStack stack_prev = new ImageStack(width, height, zs);
-      double radioAdmitido = pelar * rpre;
-      for (int i = 0; i < this.sourcePixels.length; i += this.pixelsPerSlice) {
-        byte[] slice = new byte[this.pixelsPerSlice];
-        for (int j = 0; j < this.pixelsPerSlice; j++) {
-          int pos = i + j;
-          slice[j] = distanceMap[pos] > radioAdmitido ? 0 : this.sourcePixels[pos];
-        }
-        stack_prev.setPixels(slice, 1 + i / this.pixelsPerSlice);
-      }
-
-      ZProjector projector = new ZProjector(new ImagePlus("Proyección", stack_prev));
-      projector.setMethod(ZProjector.MAX_METHOD);
-      projector.doProjection();
-      ColorProcessor icp = drawEstimations(projector.getProjection().getProcessor());
-      if (!this.previewWindowInitialized) {
-        ImagePlus impstack = new ImagePlus("Preview", icp);
-        impstack.show();
-        this.previewWindowInitialized = true;
-      }
-      else {
-        IJ.getImage().setImage(new ImagePlus("Preview", icp));
-      }
-      this.previewCache.put(new Double(pelar), icp);
+      showPreview(this.proportion);
     }
     if (okPressed) {
-      System.out.println("OK");
-      
-      //duplicar para no modificar stack original
-      ImagePlus img2 = sourceImagePlus.duplicate();
-      
-      ImageStack stack = img2.getStack();
-      
-      byte[] pixels;  //arreglo para guardar slices del stack
-      byte[] pix_est; //para estimar el centro y radio de la esfera
-      
-      int x, offset, offsetaux, pos;  //para buscar coordenadas en el stack 
-      double distancia;
-      
-      //stack auxiliar para guardar stack proyectado
-      ImageStack resultsStack = new ImageStack(width, height);    
-      
-      //centro y radio de la esfera
-      double[] x0 = new double[tiempos];
-      double[] y0 = new double[tiempos];
-      double[] z0 = new double[tiempos];
-      double[] r = new double[tiempos];
-                
-      //dimension de arreglos para guardar datos
-      int dimension = (int) width*height*zs/15;
-        
-      //arreglos para guardar datos para estimacion de la esfera
-      double[] mx = new double[dimension];
-      double[] my = new double[dimension];
-      double[] mz = new double[dimension];
-        
-      double[] datos;
-      int cnt = 0;
-      
-      ZProjector projector = new ZProjector(img2); 
-      projector.setMethod(ZProjector.MAX_METHOD);
-      
-      //proyectar para cada t
-      for(int t = 1; t <= tiempos; t++) {
-        
-        //estimar esfera
-        for(int h=1; h <= zs; h++) {
-          pix_est = (byte[]) stack.getPixels(h+(t-1)*zs);
-          for (int i=0; i < height; i++) {
-            offsetaux = i*width;
-            for (int j=0; j < width; j++) {
-              pos = offsetaux + j;
-              if ((pix_est[pos]) > threshold || pix_est[pos] < 0) {
-                mx[cnt] = j;
-                my[cnt] = i;
-                mz[cnt] = h-1;
-                cnt += 1;
-              }
-            }
-          }
-        }
-          
-        datos = sphereEstimation(mx, my, mz, cnt);
-          
-        x0[t-1] = datos[0];
-        y0[t-1] = datos[1];
-        z0[t-1] = datos[2];
-        r[t-1] = datos[3];
-        cnt = 0;
-        
-        byte[] pixel_aux;
-        byte[] pixel_aux2;
-          
-        pixel_aux = (byte[]) stack.getPixels(zs);
-                      
-        for (int h = 1; h <= zs; h++) {
-          pixels = (byte[]) stack.getPixels(h+(t-1)*zs);
-          for(int i = 0; i < height; i++) {
-            offset = i*width;
-            for(int j=0; j < width ; j++) {
-              distancia = Math.sqrt(Math.pow(((j-x0[t-1])*factorx),2) + Math.pow(((i-y0[t-1])*factory),2)
-              + Math.pow(((h-z0[t-1])*factorz),2));
-              x = offset + j;
-              if (distancia > pelar*r[t-1]) {
-                pixels[x] = 0;
-              }
-            }
-          }
-        }
-        pixel_aux2 = (byte[]) stack.getPixels(zs);
-        System.out.println("Pixel 100000 vale :"+ pixel_aux2[100000]);
-        
-        // proyectar sobre todo z para el tiempo t
-        projector.setStartSlice(t + (t-1)*zs);
-        projector.setStopSlice(t*zs);
-        projector.doProjection();  
-        ColorProcessor ipc = drawEstimations(projector.getProjection().getProcessor());
-        resultsStack.addSlice(ipc);
-      }
-      ImagePlus impstack = new ImagePlus("Result", resultsStack);
-      impstack.show();
+      processAllFrames();
     } 
+  }
+
+  private void showPreview(double proportion) {
+    if (this.previewCache.containsKey(new Double(proportion))) {
+      IJ.getImage().setImage(new ImagePlus("Preview", this.previewCache.get(new Double(proportion))));
+      return;
+    }
+    int previewTime = 1;
+    double admittedRadius = proportion * rpre;
+    ImageStack previewStack = cropSphere(previewTime, admittedRadius);
+    ZProjector projector = new ZProjector(new ImagePlus("Projection", previewStack));
+    projector.setMethod(ZProjector.MAX_METHOD);
+    projector.doProjection();
+    ColorProcessor icp = drawEstimations(projector.getProjection().getProcessor());
+    if (this.previewCache.size() == 0) {
+      ImagePlus impstack = new ImagePlus("Preview", icp);
+      impstack.show();
+    }
+    else {
+      IJ.getImage().setImage(new ImagePlus("Preview", icp));
+    }
+    this.previewCache.put(new Double(proportion), icp);
+  }
+
+  private ImageStack cropSphere(int frame, double radius) {
+    ImageStack stack = new ImageStack(this.width, this.height, this.nSlices);
+    int frameOffset = (frame - 1) * this.pixelsPerFrame;
+    for (int i = 0; i < this.pixelsPerSlice * this.nSlices; i += this.pixelsPerSlice) {
+      byte[] slice = new byte[this.pixelsPerSlice];
+      for (int j = 0; j < this.pixelsPerSlice; j++) {
+        int pos = i + j + frameOffset;
+        slice[j] = distanceMap[pos] > radius ? 0 : this.sourcePixels[pos];
+      }
+      stack.setPixels(slice, 1 + i / this.pixelsPerSlice);
+    }
+    return stack;
+  }
+
+  private void processAllFrames() {
+    System.out.println("OK");
+    
+    ImageStack resultsStack = new ImageStack(this.width, this.height, this.nSlices);
+      
+    //arreglos para guardar datos para estimacion de la esfera
+    int dimension = this.pixelsPerFrame / 15;
+    double[] mx = new double[dimension];
+    double[] my = new double[dimension];
+    double[] mz = new double[dimension];
+    
+    //proyectar para cada frame
+    for(int frame = 1; frame <= nFrames; frame++) {
+      double x, y, z, r;
+      
+      //estimar esfera
+      int cnt = 0;
+      for (int slice = 1; slice <= this.nSlices; slice++) {
+        cnt += pixelsOverThreshold(frame, slice, cnt, mx, my, mz);
+      }
+      double[] datos = sphereEstimation(mx, my, mz, cnt);
+      
+      x = datos[0];
+      y = datos[1];
+      z = datos[2];
+      r = datos[3];
+      cnt = 0;
+      
+      ImageStack sphere = cropSphere(frame, r * proportion);
+      
+      // proyectar sobre todo z para el tiempo t
+      ZProjector projector = new ZProjector(new ImagePlus("Projection", resultsStack));
+      projector.setMethod(ZProjector.MAX_METHOD);
+      projector.doProjection();  
+      ColorProcessor ipc = drawEstimations(projector.getProjection().getProcessor());
+      resultsStack.addSlice(ipc);
+    }
+    ImagePlus impstack = new ImagePlus("Result", resultsStack);
+    impstack.show();
   }
 
   private ColorProcessor drawEstimations(ImageProcessor ip) {
